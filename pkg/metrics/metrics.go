2d69571e7495b46f56b5919118d6d92c5905bccf
Lili Cosic <cosiclili@gmail.com>
2018-11-01 14:28:55 +0100

    WIP: Add new metrics pkg

---
+++ pkg/metrics/metrics.go
@@ -0,0 +1,73 @@
+package metrics
+
+import (
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	kcoll "k8s.io/kube-state-metrics/pkg/collectors"
+	"k8s.io/kube-state-metrics/pkg/metrics"
+	metricsstore "k8s.io/kube-state-metrics/pkg/metrics_store"
+
+	"github.com/operator-framework/operator-sdk/pkg/uclient"
+)
+
+// NewCollectors returns a collection of metrics in the namespaces provided, per the api/kind resource.
+// The metrics are registered in the custom generateStore function that needs to be defined.
+// Note: If namespaces are empty, all namespaces will be fetched.
+func NewCollectors(uc *UnstructuredClient,
+	namespaces []string,
+	api string,
+	kind string,
+	generateStore func(obj interface{}) []*metrics.Metric) (collectors []*kcoll.Collector) {
+
+	// TODO: what if namespaces are empty.
+	// fetch all namespaces instead
+	for _, ns := range namespaces {
+		dclient, err := uc.ClientFor(api, kind, ns)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		store := metricsstore.NewMetricsStore(generateStore)
+		reflectorPerNamespace(context.TODO(), dclient, &unstructured.Unstructured{}, store, ns)
+		collector := kcoll.NewCollector(store)
+		collectors = append(collectors, collector)
+	}
+
+	return
+}
+
+func reflectorPerNamespace(
+	ctx context.Context,
+	dynamicInterface dynamic.NamespaceableResourceInterface,
+	expectedType interface{},
+	store cache.Store,
+	ns string,
+) {
+	lw := listWatchFunc(dynamicInterface, ns)
+	reflector := cache.NewReflector(&lw, expectedType, store, 0)
+	go reflector.Run(ctx.Done())
+}
+
+func listWatchFunc(dynamicInterface dynamic.NamespaceableResourceInterface, namespace string) cache.ListWatch {
+	return cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return dynamicInterface.Namespace(namespace).List(opts)
+		},
+		WatchFunc: func(opts metav1.ListOptions) (watch.Interface, error) {
+			return dynamicInterface.Namespace(namespace).Watch(opts)
+		},
+	}
+}
